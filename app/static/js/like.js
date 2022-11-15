12ec905eecc8ebdfadadaff99f46f5b8d9546269
Nolatrae <105476991+Nolatrae@users.noreply.github.com>
2022-11-15 23:51:33 +0800

    boostrap и комментарии
    Внедрение бустрепа для работы с комментариями, пока непонятно как с этим работать - экспериментальная технология останется в комментариях на данный момент

--- app/static/js/like.js
+++ app/static/js/like.js
@@ -104,7 +104,67 @@ for (let element of like_buttons){
 // ///////////////////////////////////////////
 
 
-$('.brise-upload > label').find('input').change(function() {
-  var file = this.files;
-$('.brise-upload > label').text('Selected file: ' + file[0].name);
-})
+// $('.brise-upload > label').find('input').change(function() {
+//   var file = this.files;
+// $('.brise-upload > label').text('Selected file: ' + file[0].name);
+// })
+
+///////////////////////////////////////////////////////////////
+
+// let comments = [];
+// loadComments();
+
+// document.getElementById('comment-add').onclick = function(){
+//     event.preventDefault();
+//     let commentName = document.getElementById('comment-name');  // получили элементы формы
+//     let commentBody = document.getElementById('comment-body');  // получили элементы формы
+
+//     let comment = {
+//         name : commentName.value, //получили имя того, кто написал комментарий
+//         body : commentBody.value, // получили комментарий
+//         time : Math.floor(Date.now() / 1000) // получили время комментария
+//     }
+
+//     commentName.value = ''; //очистили форму элемента
+//     commentBody.value = ''; //очистили форму
+
+//     comments.push(comment); // добавляем в массив наш массив на 121 строке
+//     saveComments();// храним комменты
+//     showComments();// выводим на экран
+// }
+
+// function saveComments(){
+//     localStorage.setItem('comments', JSON.stringify(comments)); 
+// }
+// //сохраняем комментарии после обновления
+// function loadComments(){ 
+//     if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+//     showComments();
+// }
+
+// function showComments (){
+//     let commentField = document.getElementById('comment-field');
+//     let out = '';
+//     comments.forEach(function(item){
+//         out += `<p class="text-right small"><em>${timeConverter(item.time)}</em></p>`;//время
+//         // out += `<p class="alert alert-primary" role="alert">${item.name}</p>`;//имя
+//         out += `<p class="alert alert-success" role="alert">${item.body}</p>`;//комментарий
+//     });
+//     commentField.innerHTML = out;
+// }
+
+// //конвертируем время
+// function timeConverter(UNIX_timestamp){
+//     var a = new Date(UNIX_timestamp * 1000);
+//     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+//     var year = a.getFullYear();
+//     var month = months[a.getMonth()];
+//     var date = a.getDate();
+//     var hour = a.getHours();
+//     var min = a.getMinutes();
+//     var sec = a.getSeconds();
+//     var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+//     return time;
+//   }
+
+//   console.log(localStorage);
